efe5f9ce181d72f439cc85103f7758d176ff51da
151953c <151953c@sit.nyp.edu.sg>
2016-10-24 17:42:22 +0800

    Initial commit

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,39 @@
+package demo;
+
+/**
+ * Created by 151953c on 10/24/2016.
+ */
+public class BookDBAO {
+    // get book details given the bookId
+    // this method is dummy for now
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    // convenient method to create a dummy book detail
+    // this method is meant for testing
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+
+    public BookDetails[] getAllBooks(String bookId){
+        BookDetails[] array = new BookDetails[7];
+        for(int i=0;i<7;i++){
+            array[i] = createDummyBook(bookId);
+        }
+        return array;
+    }
+}
+
+
---
+++ src/demo/BookDetails.java
@@ -0,0 +1,89 @@
+package demo;
+
+/**
+ * Created by 151953c on 10/24/2016.
+ */
+public class BookDetails {
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public boolean isOnSale() {
+        return onSale;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    private String bookId = null;
+    private String title = null;
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setOnSale(boolean onSale) {
+        this.onSale = onSale;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    private String firstName = null;
+    private String surname = null;
+    private float price = 0.0F;
+    private boolean onSale = false;
+    private int year = 0;
+    private String description = null;
+    private int inventory = 0;
+}
